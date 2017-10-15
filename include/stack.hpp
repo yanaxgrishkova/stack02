d2fb9171138d31009cdd2540b1dcdd5c2b0e8593
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-10-15 15:29:20 +0300

    Add files via upload

---
+++ include/stack.hpp
@@ -0,0 +1,139 @@
+#include <stdexcept>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+template <typename T>
+class stack
+{
+public:
+	stack();
+	~stack();
+	stack(stack<T> const &copy);
+	stack<T>& operator = (stack<T> const &other);
+	size_t count() const;
+	void push(T const &);
+	T pop();
+	void print_array();
+	void print_last();
+	void swap();
+private:
+	T* array_;
+	size_t array_size_;
+	size_t count_;
+};
+
+template <typename T>
+stack<T>::stack() : count_(0), array_size_(0), array_(nullptr)
+{}
+
+template <typename T>
+stack<T>::stack(stack<T> const &copy)
+{
+	array_size_ = copy.array_size_;
+	count_ = copy.count_;
+	array_ = new T[array_size_];
+
+	for (int i = 0; i < array_size_; i++)
+	{
+		array_[i] = copy.array_[i];
+	}
+}
+
+template <typename T>
+stack<T>& stack<T>::operator=(stack<T> const &other)
+{
+	if (this != &other)
+	{
+		delete[] array_;
+		array_size_ = other.array_size_;
+		array_ = new T[array_size_];
+		for (int i = 0; i < array_size_; i++)
+		{
+			array_[i] = other.array_[i];
+		}
+	}
+	return *this;
+}
+
+
+template <typename T>
+stack<T>::~stack()
+{
+	delete[] array_;
+}
+
+template <typename T>
+size_t stack<T>::count() const
+{
+	return count_;
+}
+
+template <typename T>
+void stack<T>::push(T const &value)
+{
+	if (array_size_ == 0)
+	{
+		array_size_ = 1;
+		array_ = new T[array_size_];
+	}
+	else if (count_ + 1 >= array_size_)
+	{
+		array_size_ = array_size_ * 2;
+		swap();
+	}
+	array_[count_++] = value;
+}
+
+template <typename T>
+T stack<T>::pop()
+{
+	if (count_ == 0)
+		throw std::logic_error("Stack is empty! Try again!\n");
+	else
+	{
+		T temp = array_[--count_];
+
+		return temp;
+	}
+}
+
+template <typename T>
+void stack<T>::print_array()
+{
+	if (count_ == 0)
+		throw std::logic_error("Stack is empty! Try again!\n");
+	else
+		for (unsigned int i = 0; i < count_; i++)
+		{
+			std::cout << array_[i] << "\t";
+		}
+	std::cout << std::endl;
+}
+
+template <typename T>
+void stack<T>::print_last()
+{
+	if (count_ == 0)
+	{
+		throw std::logic_error("Stack is empty! Try again!\n");
+	}
+		
+	else
+	{
+		std::cout << array_[--count_] << std::endl;
+	}
+}
+
+
+template <typename T>
+void stack<T>::swap()
+{
+	T* temp = new T[array_size_];
+	std::copy(array_, array_ + count_, stdext::checked_array_iterator<T*>(temp, count_));
+
+	delete[] array_;
+
+	array_ = temp;
+}
---
+++ tests/test1.cpp
@@ -0,0 +1,24 @@
+#include <catch.hpp>
+
+#include "stack.hpp"
+
+TEST_CASE("push", "") {
+    stack<int> s1;
+    s1.push(1);
+    s1.push(2);
+    s1.push(3);
+    s1.push(4);
+    s1.push(5);
+    REQUIRE(s1.count() == 5);
+}
+
+TEST_CASE("pop", "") {
+    stack<int> s1;
+    s1.push(1);
+    s1.push(2);
+    s1.push(3);
+    s1.push(4);
+    s1.push(5);
+    s1.pop();
+    REQUIRE(s1.count() == 4);
+}
